35bff46ddd21aa6c734411446522ec9fd38d68b1
NgKiên Developer <kienlailaptrinh@gmail.com>
2026-04-25 20:31:45 +0700

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,3 +13,13 @@ document.addEventListener("mousemove",e=>{
 cursor.style.left=e.clientX+"px";
 cursor.style.top=e.clientY+"px";
 });
+// NAV SCROLL EFFECT
+window.addEventListener("scroll",()=>{
+const nav=document.querySelector(".navbar");
+
+if(window.scrollY>50){
+nav.classList.add("scrolled");
+}else{
+nav.classList.remove("scrolled");
+}
+});
